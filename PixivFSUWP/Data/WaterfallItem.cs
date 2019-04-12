558d295203354b557328fb57b0330c6e824ecd18
tobiichiamane <tobiichiamane@outlook.com>
2019-04-12 14:28:42 +0800

    初步完成Item和ViewModel

---
+++ PixivFSUWP/Data/WaterfallItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixivFSUWP.Data
+{
+    public class WaterfallItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ImageUri { get; set; }
+        public int Stars { get; set; }
+    }
+}
---
+++ PixivFSUWP/ViewModels/WaterfallItemViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PixivFSUWP.Data;
+
+namespace PixivFSUWP.ViewModels
+{
+    public class WaterfallItemViewModel
+    {
+        public int ItemId { get; private set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ImageUri { get; set; }
+        public int Stars { get; set; }
+
+        public string GetStarsString() => Stars.ToString();
+
+        public static WaterfallItemViewModel FromItem(WaterfallItem Item)
+            => new WaterfallItemViewModel()
+            {
+                ItemId = Item.Id,
+                Title = Item.Title,
+                Author = Item.Author,
+                ImageUri = Item.ImageUri,
+                Stars = Item.Stars
+            };
+    }
+}
