f224ff24e001d7821f0396c58e51cc5e86683883
tobiichiamane <tobiichiamane@outlook.com>
2019-04-23 13:56:41 +0800

    完成临摹功能（无法保存）

--- PixivFSUWP/BigImage.xaml.cs
+++ PixivFSUWP/BigImage.xaml.cs
@@ -25,6 +25,7 @@ namespace PixivFSUWP
     /// </summary>
     public sealed partial class BigImage : Page
     {
+        bool _locked = false;
         Data.BigImageDetail parameter;
         public BigImage()
         {
@@ -34,7 +35,7 @@ namespace PixivFSUWP
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             parameter = e.Parameter as Data.BigImageDetail;
-            //CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
             var view = ApplicationView.GetForCurrentView();
             view.TitleBar.ButtonBackgroundColor = Colors.Transparent;
             view.TitleBar.ButtonForegroundColor = Colors.White;
@@ -43,14 +44,69 @@ namespace PixivFSUWP
             view.Title = string.Format("{0} by {1} - {2}x{3}",
                 parameter.Title, parameter.Author,
                 parameter.Width, parameter.Height);
+            txtTitle.Text = view.Title;
             mainImg.Source = await Data.OverAll.BytesToImage(parameter.Image, parameter.Width, parameter.Height);
             base.OnNavigatedTo(e);
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            mainImg.MaxHeight = Frame.ActualHeight;
-            mainImg.MaxWidth = Frame.ActualWidth;
+            if (_locked) return;
+            gridImg.MaxHeight = scrollRoot.ActualHeight;
+            gridImg.MaxWidth = scrollRoot.ActualWidth;
+            gridImg.MinHeight = scrollRoot.ActualHeight;
+            gridImg.MinWidth = scrollRoot.ActualWidth;
+        }
+
+        void lockView()
+        {
+            _locked = true;
+            gridImg.MaxHeight = gridImg.ActualHeight;
+            gridImg.MaxWidth = gridImg.ActualWidth;
+            gridImg.MinHeight = gridImg.ActualHeight;
+            gridImg.MinWidth = gridImg.ActualWidth;
+            mainImg.Opacity = 0.3;
+            paper.MinHeight = mainImg.ActualHeight;
+            paper.MinWidth = mainImg.ActualWidth;
+            paper.MaxHeight = mainImg.ActualHeight;
+            paper.MaxWidth = mainImg.ActualWidth;
+            paper.Height = mainImg.ActualHeight;
+            paper.Width = mainImg.ActualWidth;
+            mainCanvas.MinHeight = mainImg.ActualHeight;
+            mainCanvas.MinWidth = mainImg.ActualWidth;
+            mainCanvas.MaxHeight = mainImg.ActualHeight;
+            mainCanvas.MaxWidth = mainImg.ActualWidth;
+            mainCanvas.Height = mainImg.ActualHeight;
+            mainCanvas.Width = mainImg.ActualWidth;
+        }
+
+        void unlockView()
+        {
+            _locked = false;
+            mainImg.Opacity = 1;
+            gridImg.Height = scrollRoot.ActualHeight;
+            gridImg.Width = scrollRoot.ActualWidth;
+            gridImg.MaxHeight = scrollRoot.ActualHeight;
+            gridImg.MaxWidth = scrollRoot.ActualWidth;
+            gridImg.MinHeight = scrollRoot.ActualHeight;
+            gridImg.MinWidth = scrollRoot.ActualWidth;
+        }
+
+        private void BtnDraw_Checked(object sender, RoutedEventArgs e)
+        {
+            mainCanvas.Visibility = Visibility.Visible;
+            inkToolbar.Visibility = Visibility.Visible;
+            paper.Visibility = Visibility.Visible;
+            lockView();
+        }
+
+        private void BtnDraw_Unchecked(object sender, RoutedEventArgs e)
+        {
+            mainCanvas.Visibility = Visibility.Collapsed;
+            inkToolbar.Visibility = Visibility.Collapsed;
+            paper.Visibility = Visibility.Collapsed;
+            mainCanvas.InkPresenter.StrokeContainer.Clear();
+            unlockView();
         }
     }
 }
