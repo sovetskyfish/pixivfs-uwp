cb6bfd4d951d574bdd70021f88e10a38aa6f0055
tobiichiamane <tobiichiamane@outlook.com>
2019-05-21 21:26:59 +0800

    完成Ugoira类型

---
+++ PixivFSUWP/Data/Ugoira.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace PixivFSUWP.Data
+{
+    public class Ugoira : IDisposable
+    {
+        public struct Frame
+        {
+            public BitmapImage Image;
+            public int Delay;
+        }
+
+        public List<Frame> Frames { get; } = new List<Frame>();
+
+        public void Dispose()
+        {
+            Frames.Clear();
+        }
+    }
+}
