5c49ac5a4508877967be22fba5db3befe7c27748
tobiichiamane <tobiichiamane@outlook.com>
2019-05-25 19:39:31 +0800

    因为要及时回收列表所以只能自己造后退栈

---
+++ PixivFSUWP/Data/Backstack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace PixivFSUWP.Data
+{
+    public class Backstack : Stack<(Type, object)>
+    {
+        public static Backstack Default;
+
+        public bool CanBack
+        {
+            get => base.Count > 0;
+        }
+
+        public void Push(Type Page, object Parameter)
+        {
+            base.Push((Page, Parameter));
+        }
+
+        public void Back(Frame Frame)
+        {
+            if (!CanBack) throw new InvalidOperationException();
+            (var page, var param) = base.Pop();
+            Frame.Navigate(page, param);
+        }
+    }
+}
