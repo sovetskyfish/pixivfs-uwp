5649e4d010c5e46133358577b8751f2ac98c656f
tobiichiamane <tobiichiamane@outlook.com>
2019-05-25 16:17:27 +0800

    让瀑布流项目实现INotifyPropertyChanged便于更新列表

--- PixivFSUWP/ViewModels/WaterfallItemViewModel.cs
+++ PixivFSUWP/ViewModels/WaterfallItemViewModel.cs
@@ -6,21 +6,46 @@ using System.Threading.Tasks;
 using PixivFSUWP.Data;
 using Windows.UI.Xaml.Media.Imaging;
 using System.IO;
+using System.ComponentModel;
 
 namespace PixivFSUWP.ViewModels
 {
-    public class WaterfallItemViewModel
+    public class WaterfallItemViewModel : INotifyPropertyChanged
     {
         public int ItemId { get; private set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string ImageUri { get; set; }
-        public int Stars { get; set; }
+
+        private int _stars;
+        public int Stars
+        {
+            get => _stars;
+            set
+            {
+                _stars = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Stars)));
+            }
+        }
+
         public int Pages { get; set; }
-        public bool IsBookmarked { get; set; }
+
+        private bool _isbookmarked;
+        public bool IsBookmarked
+        {
+            get => _isbookmarked;
+            set
+            {
+                _isbookmarked = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsBookmarked)));
+            }
+        }
+
         public BitmapImage ImageSource { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public async Task LoadImageAsync()
         {
