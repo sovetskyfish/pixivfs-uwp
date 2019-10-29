9eec1dae822cc38e762943df3e097161f103b39b
tobiichiamane <tobiichiamane@outlook.com>
2019-10-29 10:40:23 +0800

    添加贡献者的ViewModel

---
+++ PixivFSUWP/ViewModels/ContributorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using PixivFSUWP.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixivFSUWP.ViewModels
+{
+    public class ContributorViewModel
+    {
+        public string AvatarUrl { get; set; }
+        public string DisplayName { get; set; }
+        public string Account { get; set; }
+        public string ProfileUrl { get; set; }
+        public List<Contribution> Contributions { get; set; }
+
+        public static ContributorViewModel FromItem(Contributor Item)
+        {
+            return new ContributorViewModel()
+            {
+                AvatarUrl = Item.AvatarUrl,
+                DisplayName = Item.DisplayName,
+                Account = Item.Account,
+                ProfileUrl = Item.ProfileUrl,
+                Contributions = Item.Contributions
+            };
+        }
+    }
+}
