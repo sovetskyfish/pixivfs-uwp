f35e5d829036ffcc9a773585b2bcf29adfc2ddc2
tobiichiamane <tobiichiamane@outlook.com>
2020-06-16 13:53:50 +0800

    初步完成DownloadJob

--- PixivFSUWP/Data/DownloadManager.cs
+++ PixivFSUWP/Data/DownloadManager.cs
@@ -1,13 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace PixivFSUWP.Data
 {
+    //下载完成时的事件参数
+    public class DownloadCompletedEventArgs : EventArgs
+    {
+        public bool HasError { get; set; }
+    }
+
+    public class DownloadJob
+    {
+        public string Title { get; set; }
+        public string Uri { get; set; }
+        public string FilePath { get; set; }
+        public int Progress { get; set; }
+
+        //下载状态
+        public bool Downloading { get; private set; }
+
+        //用于暂停的ManualResetEvent
+        ManualResetEvent pauseEvent = new ManualResetEvent(true);
+
+        //用于取消任务的CancellationTokenSource
+        CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+        //下载完成时的事件
+        public event Action<DownloadJob, DownloadCompletedEventArgs> DownloadCompleted;
+
+        //进行下载
+        public async Task Download()
+        {
+            if (!Downloading)
+            {
+                Downloading = true;
+                using (var memStream = await OverAll.DownloadImage(Uri, tokenSource.Token, pauseEvent, async (loaded, length) =>
+                {
+                    await Task.Run(() =>
+                    {
+                        Progress = (int)(loaded * 100 / length);
+                    });
+                }))
+                {
+                    if (tokenSource.IsCancellationRequested) return;
+                    var file = await StorageFile.GetFileFromPathAsync(FilePath);
+                    CachedFileManager.DeferUpdates(file);
+                    using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                    {
+                        await memStream.CopyToAsync(fileStream.AsStream());
+                    }
+                    var result = await CachedFileManager.CompleteUpdatesAsync(file);
+                    if (result == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                    {
+                        DownloadCompleted?.Invoke(this, new DownloadCompletedEventArgs() { HasError = false });
+                    }
+                    else
+                    {
+                        DownloadCompleted?.Invoke(this, new DownloadCompletedEventArgs() { HasError = true });
+                    }
+                }
+            }
+        }
+
+        //暂停下载
+        public void Pause()
+        {
+            pauseEvent.Reset();
+        }
+
+        //恢复下载
+        public void Resume()
+        {
+            pauseEvent.Set();
+        }
+
+        //取消下载
+        public void Cancel()
+        {
+            tokenSource.Cancel();
+        }
+    }
+
     //静态的下载管理器。应用程序不会有多个下载管理器实例。
     public static class DownloadManager
     {
+        //下载任务列表
+        static List<DownloadJob> downloadJobs = new List<DownloadJob>();
+
     }
 }
