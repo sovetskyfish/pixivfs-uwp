27e2142b5e36cd421e8f060441612ba177ed1183
tobiichiamane <tobiichiamane@outlook.com>
2020-06-16 13:28:43 +0800

    创建DownloadManager

---
+++ PixivFSUWP/Data/DownloadManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixivFSUWP.Data
+{
+    //静态的下载管理器。应用程序不会有多个下载管理器实例。
+    public static class DownloadManager
+    {
+    }
+}
